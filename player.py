32fac77ca3c75cee747e6ed83313c47d9a846c47
Domas <domas.baronas@gmail.com>
2022-11-08 21:26:05 +0200

    Texturos, Mouse Movement

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player:
         self.game = game
         self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
+
     # MOVEMENT/Judejimas
     def movement(self):
         sin_a = math.sin(self.angle)
