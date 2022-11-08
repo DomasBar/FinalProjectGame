b5418a978a7773cb8b11b5c92d53390528d437fd
Domas <domas.baronas@gmail.com>
2022-11-08 21:10:57 +0200

    Texturos, Mouse Movement
    Texturos, Mouse Movement

---
+++ PRE_ALPHA 0.1/settings.py
@@ -0,0 +1,12 @@
+# Game Settings
+
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 60
+
+# On mini map
+Player_Pos = 1.5, 2
+Player_Angle = 0
+Player_Speed = 0.005
+Player_Rot_Speed = 0.003
+
+
---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+import pygame as pg
+from map import *
+from player import *
+from raycasting import *
+from object_renderer import *
+
+
+# Token
+# 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
+
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.mouse.set_visible(False)
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.new_game()
+
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
+        self.raycasting = RayCasting(self)
+
+    def update(self):
+        self.player.update()
+        self.raycasting.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        # self.screen.fill("black")
+        self.object_renderer.draw()
+        # self.map.draw()
+        # self.player.draw()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
