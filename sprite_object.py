936d6926ab6bd1bf59775022b66d0035ccfb6dad
Domas <domas.baronas@gmail.com>
2022-11-22 21:49:02 +0200

    Pabaigti pagražinimo žemelapio sprites

--- sprite_object.py
+++ sprite_object.py
@@ -55,7 +55,7 @@ class SpriteObject:
 
 class AnimatedSprite(SpriteObject):
     def __init__(self, game, path='resources/sprites/animated_sprites/green_light/0.png',
-                pos=(2,6), scale=0.8, shift=0.15, animation_time=120):
+                pos=(2, 8), scale=0.8, shift=0.25, animation_time=120):
         super().__init__(game, path, pos, scale, shift)
         self.animation_time = animation_time
         self.path = path.rsplit('/', 1)[0]
@@ -86,4 +86,4 @@ class AnimatedSprite(SpriteObject):
             if os.path.isfile(os.path.join(path, file_name)):
                 img = pg.image.load(path + '/' + file_name).convert_alpha()
                 images.append(img)
-            return images
+        return images
